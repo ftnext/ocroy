0afff1281fb1568d2852428d4e73d8bdb9be5e95
ftnext <takuyafjp+develop@gmail.com>
2024-01-03 14:48:56 +0900

    [feat] OCR with -m ocroy.recognizers.tesseract

--- ocroy/recognizers/tesseract.py
+++ ocroy/recognizers/tesseract.py
@@ -7,3 +7,15 @@ def recognize(image_path: str | Path) -> str:
 
     image = Image.open(image_path)
     return pytesseract.image_to_string(image, lang="jpn")
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("image_path", type=Path)
+    args = parser.parse_args()
+
+    text = recognize(args.image_path)
+
+    print(text)
