5cd33f31c7452006e59d60759e24d6d018bde08f
ftnext <takuyafjp+develop@gmail.com>
2024-01-03 15:01:01 +0900

    [feat] Normalize Tesseract OCR result

--- ocroy/recognizers/tesseract.py
+++ ocroy/recognizers/tesseract.py
@@ -2,13 +2,18 @@ from __future__ import annotations
 
 from pathlib import Path
 
+from ocroy.normalize import RemoveWhitespaceNormalizer
+
 
 def recognize(image_path: str | Path) -> str:
     import pytesseract
     from PIL import Image
 
     image = Image.open(image_path)
-    return pytesseract.image_to_string(image, lang="jpn")
+    result = pytesseract.image_to_string(image, lang="jpn")
+
+    normalizer = RemoveWhitespaceNormalizer()
+    return normalizer.normalize(result)
 
 
 if __name__ == "__main__":
--- tests/recognizers/test_tesseract.py
+++ tests/recognizers/test_tesseract.py
@@ -3,13 +3,17 @@ from unittest.mock import patch
 from ocroy.recognizers.tesseract import recognize
 
 
+@patch("ocroy.recognizers.tesseract.RemoveWhitespaceNormalizer")
 @patch("PIL.Image")
 @patch("pytesseract.image_to_string")
-def test_recognize(image_to_string, Image):
+def test_recognize(image_to_string, Image, RemoveWhitespaceNormalizer):
     image = Image.open.return_value
+    normalizer = RemoveWhitespaceNormalizer.return_value
 
     actual = recognize("path/to/image.png")
 
-    assert actual == image_to_string.return_value
+    assert actual == normalizer.normalize.return_value
     Image.open.assert_called_once_with("path/to/image.png")
     image_to_string.assert_called_once_with(image, lang="jpn")
+    RemoveWhitespaceNormalizer.assert_called_once_with()
+    normalizer.normalize.assert_called_once_with(image_to_string.return_value)
