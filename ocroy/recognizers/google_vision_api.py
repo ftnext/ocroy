bfac6bd4219f1dad3c5fab25278b469652421bdd
ftnext <takuyafjp+develop@gmail.com>
2024-01-03 14:31:20 +0900

    [feat] OCR with -m ocroy.recognizers.google_vision_api

--- ocroy/recognizers/google_vision_api.py
+++ ocroy/recognizers/google_vision_api.py
@@ -11,3 +11,15 @@ def recognize(image_path: str | Path) -> str:
     response = client.document_text_detection(image=image)
     annotations = response.text_annotations
     return annotations[0].description
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("image_path", type=Path)
+    args = parser.parse_args()
+
+    text = recognize(args.image_path)
+
+    print(text)
